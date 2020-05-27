fb013bac7f0f4172f9289f4cb97fc1507820df51
Naoya Ono <ono.naoyaa@gmail.com>
2020-05-27 17:50:30 +0800

    fix

--- packages/json-api-normalizer/src/index.ts
+++ packages/json-api-normalizer/src/index.ts
@@ -1,5 +1,46 @@
 import normalizer from 'json-api-normalizer';
 
-export * from 'json-api-normalizer';
+interface BaseResourceData {
+  id: string;
+  type: string;
+}
 
-export default normalizer;
+export interface Attributes {
+  [attribute: string]: any;
+}
+
+export interface ResourceData<A extends Attributes = Attributes>
+  extends BaseResourceData {
+  attributes: A;
+  relationships: {
+    [entity: string]: {
+      data: BaseResourceData | BaseResourceData[] | null;
+    };
+  };
+}
+
+export interface JsonApiObject<T extends ResourceData | ResourceData[] = ResourceData> {
+  data: T;
+  // FIXME: needs better typing
+  included: ResourceData[];
+  meta?: {
+    [attribute: string]: any;
+  };
+}
+
+// https://github.com/yury-dymov/json-api-normalizer#options
+export interface Opts {
+  endpoint?: boolean;
+  filterEndpoint?: boolean;
+  camelizeKeys?: boolean;
+  camelizeTypeValues?: boolean;
+}
+
+export interface NormalizedData {
+  [entity: string]: {
+    [id: string]: ResourceData;
+  };
+}
+
+const normalize: (json: JsonApiObject, opts?: Opts) => NormalizedData = normalizer;
+export default normalize;
